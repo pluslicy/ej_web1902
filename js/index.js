ff1019cfc21caff52eda62cc4512ae8abd9c1b27
plusyuyu <532323119@qq.com>
2019-09-06 14:46:43 +0800

    修复了左侧栏目

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 $(function(){
 	// 为栏目绑定事件
-
 	$("ul.left_nav").on("click","li",function(){
+		// this = event.currentTarget]
+		$("ul.left_nav > li").removeClass("current");
+		$(this).addClass("current");
 		var url = $(this).attr("url");
 		$("#wrapper").load(url)
 	})
+
+	// 默认选中第一个li
+	$("ul.left_nav > li:first-child").trigger("click");
+
+
 });
